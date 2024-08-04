0e179283e27deba45e2519aa9f81ff3bca6fdfdf
John Yu <byu9@ncsu.edu>
2024-08-04 16:35:13 -0400

    add features to MultiDiGraph

--- route_engine/graphs.py
+++ route_engine/graphs.py
@@ -1,64 +1,69 @@
 class MultiDiGraph:
     def __init__(self):
-        self._nodes = set()
-        self._parallel_edges = dict()
+        self._node_data = dict()
+        self._edge_data = dict()
+        self._node_children = dict()
+        self._node_parents = dict()
 
-    def insert_edge(self, src, dst, key, **edge_data):
+    def insert_node(self, node):
+        if node in self._node_data:
+            raise ValueError(f'Node "{node}" exists.')
 
-        if (src, dst) not in self._parallel_edges:
-            self._parallel_edges[src, dst] = {key: edge_data}
-
-        elif key in self._parallel_edges[src, dst]:
-            raise ValueError(f'Edge ({src=:}, {dst=:}, {key=:}) exists.')
-
-        else:
-            self._parallel_edges[src, dst][key] = edge_data
-
-        self._nodes.add(src)
-        self._nodes.add(dst)
+        self._node_data[node] = dict()
+        self._node_children[node] = set()
+        self._node_parents[node] = set()
 
     @property
     def nodes(self):
-        yield from self._nodes
+        yield from self._node_data.keys()
 
     def children_of(self, node):
-        """
-        Returns children of the given node.
-        :param node: Node whose children are to be returned.
-        :return: Children of the given node as a sequence, which may be empty.
-        """
-        for (src, dst) in self._parallel_edges.keys():
-            if src == node:
-                yield dst
+        if node not in self._node_children:
+            raise LookupError(f'Node "{node}" does not exist.')
 
-    def edges_between(self, src, dst):
-        """
-        Returns edges between two nodes as a sequence.
+        yield from self._node_children[node]
 
-        :param src: Source node
-        :param dst: Destination node
-        :return: Sequence of tuple(src, dst, key) each uniquely identifying an
-        edge between the two nodes. If they are not connected, an empty
-        sequence is returned.
-        """
-        if (src, dst) in self._parallel_edges:
-            for key in self._parallel_edges[src, dst].keys():
-                yield src, dst, key
+    def parents_of(self, node):
+        if node not in self._node_parents:
+            raise LookupError(f'Node "{node}" does not exist.')
 
-    def edge_data(self, edge):
-        """
-        Returns the data stored in the edge as a modifiable dictionary
-        :param edge: tuple(src, dst, key) uniquely identifying the edge
-        :return: Data stored in the edge as a modifiable dictionary
-        """
-        src, dst, key = edge
+        yield from self._node_parents[node]
 
-        if (src, dst) not in self._parallel_edges:
-            raise ValueError(f'No edges exist between {src=:} and {dst=:}')
+    def node_data(self, node):
+        if node not in self._node_data:
+            raise LookupError(f'Node "{node}" does not exist.')
 
-        if key not in self._parallel_edges[src, dst]:
-            raise ValueError(
-                f'No edge with {key=:} exists between {src=:} and {dst=:}'
-            )
+        return self._node_data[node]
 
-        return self._parallel_edges[src, dst][key]
+    def insert_edge(self, src_node, dst_node, edge_key):
+        if src_node not in self._node_data:
+            raise ValueError(f'Source node "{src_node}" does not exist.')
+
+        if dst_node not in self._node_data:
+            raise ValueError(f'Destination node "{dst_node}" does not exist.')
+
+        if (src_node, dst_node) in self._edge_data:
+            if edge_key in self._edge_data[src_node, dst_node]:
+                raise ValueError(f'Edge "({src_node}, {dst_node}, {edge_key})" exists.')
+
+            else:
+                self._edge_data[src_node, dst_node][edge_key] = dict()
+
+        else:
+            self._edge_data[src_node, dst_node] = {edge_key: dict()}
+
+        self._node_children[src_node].add(dst_node)
+        self._node_parents[dst_node].add(src_node)
+
+    @property
+    def edges(self):
+        for (src_node, dst_node), data_dicts in self._edge_data.items():
+            for edge_key in data_dicts.keys():
+                yield src_node, dst_node, edge_key
+
+    def edge_data(self, src_node, dst_node, edge_key):
+        if (src_node, dst_node) in self._edge_data:
+            if edge_key in self._edge_data[src_node, dst_node]:
+                return self._edge_data[src_node, dst_node][edge_key]
+
+        raise ValueError(f'Edge "({src_node}, {dst_node}, {edge_key})" does not exist.')
--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -3,31 +3,58 @@ import unittest
 from route_engine.graphs import MultiDiGraph
 
 
-class TestGraph(unittest.TestCase):
+class TestMultiDiGraph(unittest.TestCase):
 
     def test1(self):
         graph = MultiDiGraph()
-        graph.insert_edge(src='a', dst='b', key='primary', weight=1)
-        graph.insert_edge(src='a', dst='b', key='secondary', weight=2)
-        graph.insert_edge(src='a', dst='c', key='primary', weight=5)
-        graph.insert_edge(src='b', dst='a', key='primary', weight=6)
 
+        graph.insert_node('a')
+        graph.insert_node('b')
+        graph.insert_node('c')
+
+        graph.node_data('a')['name'] = 'a'
+        graph.node_data('b')['name'] = 'b'
+        graph.node_data('c')['name'] = 'c'
+
+        self.assertEqual(graph.node_data('a')['name'], 'a')
+        self.assertEqual(graph.node_data('b')['name'], 'b')
+        self.assertEqual(graph.node_data('c')['name'], 'c')
+
+        # Insert duplicate nodes must raise error
         with self.assertRaises(ValueError):
-            graph.insert_edge(src='a', dst='b', key='primary', weight=1)
+            graph.insert_node('a')
+
+        graph.insert_edge(src_node='a', dst_node='b', edge_key='0')
+        graph.insert_edge(src_node='a', dst_node='b', edge_key='1')
+        graph.insert_edge(src_node='a', dst_node='c', edge_key='0')
+        graph.insert_edge(src_node='b', dst_node='a', edge_key='0')
+
+        graph.edge_data('a', 'b', '0')['name'] = 'e0'
+        graph.edge_data('a', 'b', '1')['name'] = 'e1'
+        graph.edge_data('a', 'c', '0')['name'] = 'e2'
+        graph.edge_data('b', 'a', '0')['name'] = 'e3'
+
+        self.assertEqual(graph.edge_data('a', 'b', '0')['name'], 'e0')
+        self.assertEqual(graph.edge_data('a', 'b', '1')['name'], 'e1')
+        self.assertEqual(graph.edge_data('a', 'c', '0')['name'], 'e2')
+        self.assertEqual(graph.edge_data('b', 'a', '0')['name'], 'e3')
+
+        # Inserting duplicate edges must raise error
+        with self.assertRaises(ValueError):
+            graph.insert_edge(src_node='a', dst_node='b', edge_key='0')
 
         self.assertCountEqual(graph.nodes, ['a', 'b', 'c'])
+        self.assertCountEqual(graph.edges, [
+            ('a', 'b', '0'),
+            ('a', 'b', '1'),
+            ('a', 'c', '0'),
+            ('b', 'a', '0')
+        ])
 
         self.assertCountEqual(graph.children_of('a'), ['b', 'c'])
         self.assertCountEqual(graph.children_of('b'), ['a'])
         self.assertCountEqual(graph.children_of('c'), [])
 
-        self.assertCountEqual(graph.edges_between('a', 'b'), [
-            ('a', 'b', 'primary'),
-            ('a', 'b', 'secondary')
-        ])
-
-        self.assertCountEqual(graph.edges_between('a', 'c'), [('a', 'c', 'primary')])
-        self.assertCountEqual(graph.edges_between('b', 'c'), [])
-
-        self.assertEqual(graph.edge_data(('a', 'b', 'secondary')), {'weight': 2})
-        self.assertEqual(graph.edge_data(('a', 'c', 'primary')), {'weight': 5})
+        self.assertCountEqual(graph.parents_of('a'), ['b'])
+        self.assertCountEqual(graph.parents_of('b'), ['a'])
+        self.assertCountEqual(graph.parents_of('c'), ['a'])
